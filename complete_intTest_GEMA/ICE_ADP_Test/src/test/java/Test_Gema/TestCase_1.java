27ff51386a8a556de88db95e3c091c5ebdf193ce
stefanorm <stefano.tofani@catenate.com>
2017-09-13 20:08:00 +0200

    created the complete citrus TestCase project

---
+++ complete_intTest_GEMA/ICE_ADP_Test/src/test/java/Test_Gema/TestCase_1.java
@@ -0,0 +1,16 @@
+package Test_Gema;
+
+import org.testng.annotations.Test;
+
+import com.consol.citrus.annotations.CitrusXmlTest;
+import com.consol.citrus.testng.AbstractTestNGCitrusTest;
+
+/**
+
+ */
+@Test
+public class TestCase_1 extends AbstractTestNGCitrusTest {
+
+    @CitrusXmlTest(name = "TestCase_1")
+    public void testCase1Gema() {}
+}
