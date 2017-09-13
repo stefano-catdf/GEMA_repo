e3326f2fc9db94e484b6ae4ed45260c3e829521b
tibco <tibco@oracledatabase12c.(none)>
2017-09-13 11:33:11 +0200

    first commit _citrus Project

---
+++ GEMA_INT-TEST/testjms/src/test/java/com/gema/testjms/SoapJMS_MaxEai_TEST.java
@@ -0,0 +1,12 @@
+package com.gema.testjms;
+
+import com.consol.citrus.annotations.CitrusXmlTest;
+import com.consol.citrus.testng.AbstractTestNGCitrusTest;
+import org.testng.annotations.Test;
+
+@Test
+public class SoapJMS_MaxEai_TEST extends AbstractTestNGCitrusTest {
+    @CitrusXmlTest(name = "SoapJMS_MaxEai_TEST")
+    public void soapJMSMaxEaiOk() {}
+}
+ o
